2ad20a9b9c29c6d3df193b9c7415442a6fa7c071
AndreanLNA <83387525+AndreanLNA@users.noreply.github.com>
2021-12-22 21:33:53 +0700

    Update A)KodeProgramEDGambar.py

--- A)KodeProgramEDGambar.py
+++ A)KodeProgramEDGambar.py
@@ -1,8 +1,9 @@
+# Import library
 import os
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES //import AES
 
-
+# Generate nilai kunci atau Key
 def getKey(keysize):
     key = os.urandom(keysize)
     return key
@@ -12,14 +13,15 @@ def getIV(blocksize):
     iv = os.urandom(blocksize)
     return iv
 
-//program enkripsi foto
+# program enkripsi foto
 def encrypt_image(filename, key, iv):
-    BLOCKSIZE = 16
-    encrypted_filename = "Hasil_Enkripsi_" + filename   //menentukan nama file yang telah dienkripsi
+    BLOCKSIZE = 16  # Panjang blok dari key yang digunakan
+    encrypted_filename = "Hasil_Enkripsi_" + filename   #menentukan nama file yang telah dienkripsi
 
     with open(filename, "rb") as file1:
         data = file1.read()
-
+        
+        # Penggunaan AES
         cipher = AES.new(key, AES.MODE_CBC, iv)
         ciphertext = cipher.encrypt(pad(data, BLOCKSIZE))
 
@@ -27,10 +29,10 @@ def encrypt_image(filename, key, iv):
             file2.write(ciphertext)
     return encrypted_filename
 
-//Program deskripsi foto dari hasil enkripsi di atas
+#Program deskripsi foto dari hasil enkripsi di atas
 def decrypt_image(filename, key, iv):
     BLOCKSIZE = 16
-    decrypted_filename = "Hasil_Deskripsi_" + filename     //menentukan nama file yang telah di deskripsi
+    decrypted_filename = "Hasil_Deskripsi_" + filename     #menentukan nama file yang telah di deskripsi
 
     with open(filename, "rb") as file1:
         data = file1.read()
@@ -43,13 +45,13 @@ def decrypt_image(filename, key, iv):
 
     return decrypted_filename
 
+ 
+KEYSIZE = 16  # Key yang digunakan
+BLOCKSIZE = 16   # Panjang blok dari key yang digunakan
+filename = "Andrean.JPG"        #Memasukkan nama foto yang akan dienkripsi
 
-KEYSIZE = 16
-BLOCKSIZE = 16
-filename = "Andrean.JPG"         //Memasukkan nama foto yang akan dienkripsi
-
-key = getKey(KEYSIZE)
-iv = getIV(BLOCKSIZE)
+key = getKey(KEYSIZE)   # Variabel untuk mengambil dari nilai KEYSZISE
+iv = getIV(BLOCKSIZE)   # Variabel untuk mengambil dari nilai BLOCKSIZE
 
 encrypted_filename = encrypt_image(filename, key, iv)
 decrypted_filename = decrypt_image(encrypted_filename, key, iv)
